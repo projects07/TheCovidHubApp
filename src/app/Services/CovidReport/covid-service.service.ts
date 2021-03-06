17b643f4d1d0daa9f255add950923ec92444c895
Gowtham0499 <gowth0499@gmail.com>
2021-03-06 15:11:58 +0530

    modified

--- src/app/Services/CovidReport/covid-service.service.ts
+++ src/app/Services/CovidReport/covid-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,10 +8,18 @@ export class CovidServiceService {
 
   constructor(private http: HttpClient) { }
   public getCovidData(){
-    return this.http.get("https://corona.lmao.ninja/v2/countries");
+    const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Headers', '*');
+    headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    return this.http.get("https://corona.lmao.ninja/v2/countries", { headers: headers });
   }
   getHistoricalData(fromDate:string, toDate:string,Country:string){
+    const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Headers', '*');
+    headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     return this.http.get("https://api.covid19api.com/country/"+Country +"?from="+
-    fromDate+"T00:00:00Z&to="+toDate+"T00:00:00Z")
+    fromDate+"T00:00:00Z&to="+toDate+"T00:00:00Z", { headers: headers })
   }
 }
